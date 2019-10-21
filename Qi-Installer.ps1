42762d42ee959d32ed4891e772aa819ed02d8409
David Klatka <daveklatka@gmail.com>
2019-10-21 16:59:09 -0400

    tls12

--- Qi-Installer.ps1
+++ Qi-Installer.ps1
@@ -5,4 +5,26 @@ if (!($PSScriptRoot -match $env:SystemDrive)) {
 } else {
     $ScriptPath = "$env:systemDrive\QiInstaller"
 }
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
 (New-Object System.Net.WebClient).DownloadString('https://gitlab.com/api/v4/projects/14874591/repository/files/Qi_Installer%2Eps1/raw?ref=master') | Invoke-Expression;
+
+
+
+<#
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
+function fixuri($uri){
+    $type = $uri.GetType();
+    $fieldInfo = $type.GetField('m_Syntax', ([System.Reflection.BindingFlags]::Instance -bor [System.Reflection.BindingFlags]::NonPublic));
+    $uriParser = $fieldInfo.GetValue($uri);
+    $typeUriParser = $uriParser.GetType().BaseType;$fieldInfo = $typeUriParser.GetField('m_Flags', ([System.Reflection.BindingFlags]::Instance -bor [System.Reflection.BindingFlags]::NonPublic -bor [System.Reflection.BindingFlags]::FlattenHierarchy));
+    $uriSyntaxFlags = $fieldInfo.GetValue($uriParser);
+    $uriSyntaxFlags = $uriSyntaxFlags -band (-bnot 0x2000000);
+    $uriSyntaxFlags = $uriSyntaxFlags -band (-bnot 0x20000);
+    $fieldInfo.SetValue($uriParser, $uriSyntaxFlags);
+};
+$uri = New-Object System.Uri -ArgumentList ('https://gitlab.com/api/v4/projects/14874591/repository/files/Qi_Installer%2Eps1/raw?ref=master');
+fixuri $uri;
+$PrivateToken = 'iZukjgy--jToWBo5Xe1t';
+$Header = @{'PRIVATE-TOKEN' = $PrivateToken};
+Invoke-restmethod -Headers $Header -Method Get -Uri $URI | Invoke-Expression;
+#>
