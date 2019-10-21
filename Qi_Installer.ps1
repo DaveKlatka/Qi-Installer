94755a5b64c0b1ed0bcccd1ae7216fef46f56ccc
David Klatka <daveklatka@gmail.com>
2019-10-21 16:46:19 -0400

    test ready

--- Qi_Installer.ps1
+++ Qi_Installer.ps1
@@ -16,8 +16,7 @@ $AuthSubmit_Click = {
         Connect-AutomateAPI -credential $Credential -Server $authServer -TwoFactorToken $2FAAuth.Text -ErrorAction stop
 
         $Script:Location = (get-automateclient -clientname "1_Technician Catchall").Locations | Where-Object { $_.ScriptExtra1 -eq $AuthUser.text }
-        $TechInstaller.Text = [System.String]"Tech Installer ($($Location.name)) $($Location.ID)"
-        write-host $location
+        $TechInstaller.Text = [System.String]"Tech Installer ($($Location.name))"
 
         if (!($null -eq $Location.ID)) {
             $AuthPanel.Visible = $false
