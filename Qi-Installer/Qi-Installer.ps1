576a490af38bd5cdfe9b38f53ae57d8276915caf
David Klatka <daveklatka@gmail.com>
2019-10-21 14:47:26 -0400

    folders

--- Qi-Installer/Qi-Installer.ps1
+++
@@ -1,7 +0,0 @@
-
-if (!($PSScriptRoot -match $env:SystemDrive)) {
-    $ScriptPath = $PSScriptRoot
-} else {
-    $ScriptPath = "$env:systemDrive\QiInstaller"
-}
-(New-Object System.Net.WebClient).DownloadString('https://gitlab.com/api/v4/projects/14874591/repository/files/Qi-Installer%2FQi_Installer%2Eps1/raw?ref=master') | Invoke-Expression;
