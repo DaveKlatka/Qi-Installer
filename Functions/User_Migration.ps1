19c88f6f247d0ee9115d7a682e6629eb8bc48479
David Klatka <daveklatka@gmail.com>
2019-11-04 16:28:12 -0500

    usmt debugging

--- Functions/User_Migration.ps1
+++ Functions/User_Migration.ps1
@@ -138,16 +138,9 @@ function Get-USMT {
     }
     else {
         Update-Textbox "USMT not on local machine. Downloading binaries."
-        $Script:output = $ScriptPath
-        $Script:Source = "$DownloadHost/AutoMate/Tools/User_State_Migration_Tool.zip"
-        $Script:Destination = "$output\User_State_migration_Tool.zip"
-        if (!(Test-Path $output)) { New-Item -ItemType Directory -Path $output }
-        Import-Module BitsTransfer
-        Start-BitsTransfer -Source $Source -Destination $Destination
-        $Script:file = $Destination
-        $Script:ExtractDestination = $output
-        Start-Extract
-        Remove-Item -Path "$file*" -Recurse
+        Get-Files -Source "$DownloadHost/AutoMate/Tools/User_State_Migration_Tool.zip" -Destination "$ScriptPath\User_State_migration_Tool.zip" -NumberOfFiles 1 -Software "User State Migration Tool"
+        Start-Extract -File "$ScriptPath\User_State_migration_Tool.zip" -ExtractTo $ScriptPath
+        Remove-Item -Path "$ScriptPath\User_State_migration_Tool.zip" -Recurse
         $Script:ScanState = "$USMTPath\scanstate.exe"
         $Script:LoadState = "$USMTPath\loadstate.exe"
     }
