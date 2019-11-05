f7892607e107957df4bc1226218d203263014418
David Klatka <daveklatka@gmail.com>
2019-11-05 15:52:43 -0500

    debug loadstate

--- Functions/User_Migration.ps1
+++ Functions/User_Migration.ps1
@@ -139,7 +139,7 @@ function Restore-UserState {
     Update-Textbox "`nBeginning migration..."
     
     # Get the location of the save state data
-    $Script:Destination = "$($SaveSourceTextBox.Text)"
+    $Destination = "$($SaveSourceTextBox.Text)"
 
     # Check that the save state data exists
     if (!(Test-Path (Get-Childitem -Path $Destination -include *.MIG -recurse).FullName)) {
