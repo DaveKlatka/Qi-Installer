66dcb047af645eb105804d9e47ee98ee253ee3c2
David Klatka <daveklatka@gmail.com>
2020-01-09 09:07:59 -0500

    7za temp

--- Qi_Installer.ps1
+++ Qi_Installer.ps1
@@ -130,7 +130,7 @@ function Invoke-Extract {
         [string] $ExtractTo
     )
     $Source = "$DownloadHost/AutoMate/Tools/7za.exe"
-    $7zip = "$Scriptpath\7za.exe"
+    $7zip = "$env:TEMP\7za.exe"
     if (!(Test-Path $7zip)) {
         Update-LogBox "Downloading 7zip extractor"
         $CurrentFile.Value = 0
